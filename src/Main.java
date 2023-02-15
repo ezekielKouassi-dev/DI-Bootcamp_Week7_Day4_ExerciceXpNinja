c920668c7e85052f58cafdd7c9e6809a042f7ec6
ezekielkouassi <121268728+ezekielKouassi-dev@users.noreply.github.com>
2023-02-15 23:35:54 +0000

    first commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        LinkedList<String> linkedList = new LinkedList<>();
+        linkedList.add("Merci");
+        linkedList.add("Gentille");
+        linkedList.add("Reconnaissant");
+        linkedList.add("Anxieux");
+        linkedList.add("Joyeux");
+
+        linkedList.get(2);
+
+        linkedList.remove(3);
+
+        linkedList.get(3);
+    }
+}
---
+++ src/Node.java
@@ -0,0 +1,18 @@
+/**
+ * Linked list Node.
+ *
+ * @param <E>
+ */
+class Node<E> {
+    E data;
+    Node<E> next;
+
+    int index;
+
+    public Node(E d) {
+        data = d;
+        next = null;
+        index = 0;
+    }
+
+}
